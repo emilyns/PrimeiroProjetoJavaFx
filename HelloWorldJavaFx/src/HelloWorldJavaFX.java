a5844dec8d44d7903ce400549233ed11241f5635
Emily Nascimento <emilly004@gmail.com>
2018-11-23 17:05:58 -0300

    Adicionando cores e reposicionando elementos

--- HelloWorldJavaFx/src/HelloWorldJavaFX.java
+++ HelloWorldJavaFx/src/HelloWorldJavaFX.java
@@ -1,6 +1,7 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
@@ -17,9 +18,14 @@ public class HelloWorldJavaFX extends Application{
     @Override
     public void start(Stage palco){
         //Personagem
-      Rectangle ceu = new Rectangle(900, 450);
-      Rectangle mar = new Rectangle(900, 450);
-      Circle sol = new Circle(90);
+      Rectangle ceu = new Rectangle(900, 450, Color.DARKORANGE);
+      Rectangle mar = new Rectangle(900, 450, Color.LIGHTSKYBLUE);
+      Circle sol = new Circle(90, Color.YELLOW);
+        //posicionamento
+        ceu.setTranslateY(-200);
+        mar.setTranslateY(200);
+        sol.setTranslateY(50);
+        
       
         //história
       StackPane historia = new StackPane();
@@ -28,6 +34,7 @@ public class HelloWorldJavaFX extends Application{
       Scene cena = new Scene(historia, 900, 900);
         //colocando a cena no Palco
       palco.setScene (cena);
+      palco.setTitle("Patamares");
       palco.show();
       
       
